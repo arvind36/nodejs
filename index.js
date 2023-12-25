f34a9135df0b8725cc424d14024f0e68fdb4b660
arvind <arvindsharan46@gmail.com>
2023-12-25 17:23:38 +0530

    done with ejs

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const publicPath = path.join(__dirname, "public");
 
-// app.use(express.static(publicPath));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.sendFile(`${publicPath}/index.html`);
@@ -15,7 +15,22 @@ app.get("/about", (req, res) => {
 app.get("/contact", (req, res) => {
   res.sendFile(`${publicPath}/contact.html`);
 });
+app.get("/profile", (req, res) => {
+  const user = {
+    name: "Arvind",
+    email: "arvind@gmail.com",
+    phone: 1234567890,
+    city: "bangalore",
+    skills: ["html", "css", "js", "react", "mongoDB", "nodejs"],
+  };
+  res.render("profile", { user });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 app.get("*", (req, res) => {
   res.sendFile(`${publicPath}/pnf.html`);
 });
+
 app.listen(5000);
