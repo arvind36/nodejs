236df6a5fd9742f360d0b6df0e3e193c45261b58
arvind <arvindsharan46@gmail.com>
2023-12-25 14:07:37 +0530

    done with routing

--- routing.js
+++ routing.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, This is Home page");
+  console.log("Data swnt by browser ==>", req.query.name);
+  res.send("Welcome " + req.query.name);
 });
 
 app.get("/about", (req, res) => {
