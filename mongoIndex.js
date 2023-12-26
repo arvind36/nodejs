b7841d8415a08567de5331d15ed05b0c69ba2b09
arvind <arvindsharan46@gmail.com>
2023-12-26 12:37:45 +0530

    read data in mongodb

--- mongoIndex.js
+++ mongoIndex.js
@@ -1,13 +1,17 @@
-const { MongoClient } = require("mongodb");
-const url = "mongodb://localhost:27017";
-const database = "e-comm";
-const client = new MongoClient(url);
+const dbConnect = require("./mongodb");
+// dbConnect().then((res) => {
+//   res
+//     .find()
+//     .toArray()
+//     .then((data) => {
+//       console.warn(data);
+//     });
+// });
+// console.log(dbConnect());
 
-async function getData() {
-  let result = await client.connect();
-  let db = result.db(database);
-  let collection = db.collection("products");
-  let response = await collection.find({}).toArray();
-  console.log(response);
-}
-getData();
+const main = async () => {
+  let data = await dbConnect();
+  data = await data.find().toArray();
+  console.warn(data);
+};
+main();
