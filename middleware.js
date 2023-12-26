6f6020b0a65ab13e854847e55b961e5ee1297141
arvind <arvindsharan46@gmail.com>
2023-12-26 12:16:55 +0530

    done with mongo connect

--- middleware.js
+++
@@ -1,9 +0,0 @@
-module.exports = reqFilter = (req, res, next) => {
-  if (!req.query.age) {
-    res.send("please provide age");
-  } else if (req.query.age < 18) {
-    res.send("You cannot access this page");
-  } else {
-    next();
-  }
-};
