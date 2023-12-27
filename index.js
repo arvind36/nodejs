837425a6c5af6340bbac7fc7eb1621c3f8b9bc95
arvind <arvindsharan46@gmail.com>
2023-12-27 09:56:04 +0530

    done with post api in mongoose

--- index.js
+++ index.js
@@ -1,46 +1,16 @@
-const mongoose = require("mongoose");
+const express = require("express");
+require("./config"); // Assuming this is the correct path to your configuration file
+const Product = require("./product");
 
-mongoose.connect("mongodb://localhost:27017/e-comm");
-const ProductsSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  brand: String,
-  category: String,
+const app = express();
+app.use(express.json());
+app.post("/create", async (req, res) => {
+  let data = new Product(req.body);
+  let result = await data.save();
+  console.log(result);
+  res.send(result);
 });
 
-const saveInDB = async () => {
-  const Product = mongoose.model("products", ProductsSchema);
-  let data = new Product({
-    name: "realme narzo 30",
-    price: "850",
-    brand: "realme",
-    category: "mobile",
-  });
-  let result = await data.save();
-};
-// saveInDB();
-
-const updateInDB = async () => {
-  const Product = mongoose.model("products", ProductsSchema);
-  let data = await Product.updateOne(
-    { name: "iphone 12" },
-    { $set: { brand: "apple" } }
-  );
-  console.log(data);
-};
-
-// updateInDB();
-
-const deleteInDB = async () => {
-  const Product = mongoose.model("products", ProductsSchema);
-  let data = await Product.deleteOne({ name: "realme narzo 30" });
-  console.log(data);
-};
-
-const findInDB = async () => {
-  const Product = mongoose.model("products", ProductsSchema);
-  let data = await Product.find({ name: "realme narzo 30" });
-  console.log(data);
-};
-
-findInDB();
+app.listen(4500, () => {
+  console.log("server running on 4500");
+});
